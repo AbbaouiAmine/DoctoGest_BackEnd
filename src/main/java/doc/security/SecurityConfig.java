0b1b9a1f9d94e5e54eb38178cf5349a11c1e98d6
Amine Abbaoui <abbaouiamine.r@gmail.com>
2020-02-23 16:20:30 +0100

    format version

--- src/main/java/doc/security/SecurityConfig.java
+++ src/main/java/doc/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 			http.csrf().disable();
 			http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-			http.authorizeRequests().antMatchers("/login/**","/register/**").permitAll();
+			http.authorizeRequests().antMatchers("/login/**","/register/**","/annonces").permitAll();
 			http.authorizeRequests().antMatchers("/appUsers/**","/appRoles/**").hasAuthority("ADMIN");
 			http.authorizeRequests().anyRequest().authenticated();
 			http.addFilter(new JWTFilter(authenticationManager()));
